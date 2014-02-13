9064b43d58d3107feb2523692e20b4fda6297441
gusgol <gustavo.goldhardt@gmail.com>
2014-02-13 17:58:18 -0200

    Version 2.0 - Added animations and speech bubble

---
+++ javascript/default.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+	//	
+	 $(".facebook").mouseover(function(){
+	     $('body').css('background-image', 'url(image/fb.png)');
+	     $("#quote").text("Maybe you want to poke me...");
+	 });
+	 $(".facebook").mouseout(function(){
+	     $('body').css('background-image', 'none');
+	     $("#quote").text("Hi, I'm Gustavo! Want to know me better?");
+	 });
+	 $(".twitter").mouseover(function(){
+	     $('body').css('background-image', 'url(image/twitter.png)');
+	     $("#quote").text("Leave me a nice message?");
+	 });
+	 $(".twitter").mouseout(function(){
+	     $('body').css('background-image', 'none');
+	     $("#quote").text("Hi, I'm Gustavo! Want to know me better?");
+	 });
+	 $(".instagram").mouseover(function(){
+	     $('body').css('background-image', 'url(image/instagram.png)');
+	     $("#quote").text("Or check out my latest pics!");
+	 });
+	 $(".instagram").mouseout(function(){
+	     $('body').css('background-image', 'none');
+	     $("#quote").text("Hi, I'm Gustavo! Want to know me better?");
+	 });
+	 $(".linkedin").mouseover(function(){
+	     $('body').css('background-image', 'url(image/linkedin.png)');
+	     $("#quote").text("Or even hire me!");
+	 });
+	 $(".linkedin").mouseout(function(){
+	     $('body').css('background-image', 'none');
+	     $("#quote").text("Hi, I'm Gustavo! Want to know me better?");
+	 });
+	 $(".mail").mouseover(function(){
+	     $('body').css('background-image', 'url(image/mail.png)');
+	     $("#quote").text("Or just send me an e-mail...");
+	 });
+	 $(".mail").mouseout(function(){
+	     $('body').css('background-image', 'none');
+	     $("#quote").text("Hi, I'm Gustavo! Want to know me better?");
+	 });
+});
